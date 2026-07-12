dbbe8d10652c4a13d73640493aea0e025690c66f
bo4ka <ip0804019@gmail.com>
2026-07-12 20:41:03 +0700

    test(bll): add TestHelper framework and EmployeeService search tests

---
+++ backend/ProjectManager/ProjectManager.Tests/Helpers/TestHelper.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging.Abstractions;
+using ProjectManager.BLL.Mapping;
+using ProjectManager.DAL;
+
+namespace ProjectManager.Tests.Helpers
+{
+    public static class TestHelper
+    {
+        public static AppDbContext CreateInMemoryDbContext()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            var context = new AppDbContext(options);
+            context.Database.EnsureCreated();
+            return context;
+        }
+
+        public static IMapper CreateRealMapper()
+        {
+            var config = new MapperConfiguration(
+                (Action<IMapperConfigurationExpression>)(cfg =>
+                {
+                    cfg.AddMaps(typeof(MappingProfile).Assembly);
+                }),
+                NullLoggerFactory.Instance 
+            );
+
+            return config.CreateMapper();
+        }
+    }
+}
---
+++ backend/ProjectManager/ProjectManager.Tests/Services/EmployeeServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using ProjectManager.BLL.DTOs.Employee;
+using ProjectManager.BLL.Services.Employee;
+using ProjectManager.DAL.Entities;
+using ProjectManager.Tests.Helpers;
+using Xunit;
+
+namespace ProjectManager.Tests.Services
+{
+    public class EmployeeServiceTests
+    {
+        [Fact]
+        public async System.Threading.Tasks.Task SearchAsync_ShouldReturnMatchingEmployees_WhenTermMatchesPartially()
+        {
+            // Arrange
+            using var context = TestHelper.CreateInMemoryDbContext();
+            var mapper = TestHelper.CreateRealMapper();
+
+            context.Employees.AddRange(new List<Employee>()
+            {
+                new Employee { Id = 1, FirstName = "Ivan", LastName = "Ivanov", Email = "ivan@test.com", MiddleName = "Ivanovich" },
+                new Employee { Id = 2, FirstName = "Petr", LastName = "Petrov", Email = "petr@test.com", MiddleName = "Petrovich" },
+                new Employee { Id = 3, FirstName = "Sidor", LastName = "Sidorov", Email = "sidor@ivan-company.com", MiddleName = "Sidorovich" }
+            });
+            await context.SaveChangesAsync();
+
+            var service = new EmployeeService(context, mapper);
+
+            // Act
+            var result = await service.SearchAsync("ivan");
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().HaveCount(2);
+            result.Select(e => e.Id).Should().Contain(new[] { 1, 3 });
+            result.Select(e => e.Id).Should().NotContain(2);
+        }
+    }
+}
